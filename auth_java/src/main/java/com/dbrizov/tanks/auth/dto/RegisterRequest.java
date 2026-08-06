6d62405c82292c447f99dd9063fb3a3e1d827528
dbrizov <d.b.rizov@gmail.com>
2026-08-06 19:13:49 +0300

    Delete auth_java

--- auth_java/src/main/java/com/dbrizov/tanks/auth/dto/RegisterRequest.java
+++
@@ -1,9 +0,0 @@
-package com.dbrizov.tanks.auth.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record RegisterRequest(
-                @NotBlank @Size(min = 3, max = 20) String username,
-                @NotBlank @Size(min = 6, max = 72) String password) {
-}
--- auth_java/src/main/java/com/dbrizov/tanks/auth/dto/TokenResponse.java
+++
@@ -1,4 +0,0 @@
-package com.dbrizov.tanks.auth.dto;
-
-public record TokenResponse(String token) {
-}
